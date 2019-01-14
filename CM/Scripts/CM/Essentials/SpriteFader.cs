35cd5eb7d9486e4d4dc29f215fa5c508fbf6bd0b
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-01-14 12:30:06 +0100

    Added sprite fader

---
+++ CM/Scripts/CM/Essentials/SpriteFader.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CM.Essentials
+{
+	public class SpriteFader : MonoBehaviour
+	{
+		public float reduceValue = 0.01f;
+
+		private SpriteRenderer spriteRenderer;
+
+		private void Awake()
+		{
+			spriteRenderer = GetComponent<SpriteRenderer>();
+		}
+
+		private void Update()
+		{
+			Color color = spriteRenderer.color;
+			color.a -= reduceValue;
+			spriteRenderer.color = color;
+
+
+		}
+	}
+}
---
+++ CM/Scripts/CM/Spawner/SpawnAtCurrentTransform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CM.Spawner
+{
+	public class SpawnAtCurrentTransform : MonoBehaviour
+	{
+		public GameObject objectToSpawn;
+
+		public void Spawn(float angle)
+		{
+			Spawning();
+		}
+
+		public void Spawn(int seconds)
+		{
+			StartCoroutine(SpawnRoutine(seconds));
+		}
+
+		private IEnumerator SpawnRoutine(int seconds)
+		{
+			yield return new WaitForSeconds(seconds);
+			Spawning();
+		}
+
+		private void Spawning()
+		{
+			GameObject spawningObject = Instantiate(objectToSpawn);
+			spawningObject.transform.position = transform.position;
+			spawningObject.transform.rotation = transform.rotation;
+		}
+	}
+}
