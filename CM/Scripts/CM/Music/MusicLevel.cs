01a7118ffadf3f804d338f86d796c469deab5050
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-01-18 14:23:19 +0100

    Added Music

---
+++ CM/Scripts/CM/Music/MusicLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CM.Music
+{
+	[CreateAssetMenu(menuName = "CM/Music/New Music Level", fileName = "NewMusicLevel.asset")]
+	public class MusicLevel : ScriptableObject
+	{
+		public int bpm;
+		public AudioClip audio;
+		public float audioVolume = 1;
+		public float audioPitch = 1;
+		public float cameraSize = 5;
+		public List<Beat> beats;
+	}
+}
---
+++ CM/Scripts/CM/Music/RhythmController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace CM.Music
+{
+	public class RhythmController : MonoBehaviour
+	{
+		[SerializeField]
+		private MusicLevel _level;
+		public MusicLevel Level { get => _level; }
+
+		public delegate void BeatHandler(int beat);
+		public event BeatHandler BeatEvent;
+
+		private int _currentBeat = 0;
+		public int CurrentBeat { get => _currentBeat; }
+
+		private float _secondsPerBeat;
+		public float SecondsPerBeat { get => _secondsPerBeat; }
+
+		private void Awake()
+		{
+			_secondsPerBeat = 60f / _level.bpm;
+			Camera.main.orthographicSize = _level.cameraSize;
+		}
+
+		private void Start()
+		{
+			InvokeRepeating("NextBeat", 0, _secondsPerBeat * 0.25f);
+		}
+
+		private void NextBeat()
+		{
+			BeatEvent?.Invoke(_currentBeat);
+
+			_currentBeat++;
+		}
+	}
+}
