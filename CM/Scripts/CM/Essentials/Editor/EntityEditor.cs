12a924809428dc9bf568eb5cbf12eb72cc9e9320
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-07-12 01:51:51 +0200

    Added an Entity class

---
+++ CM/Scripts/CM/Essentials/Editor/EntityEditor.cs
@@ -0,0 +1,21 @@
+﻿using CM.Essentials;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Entity), true)]
+public class EntityEditor : Editor
+{
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+		Debug.Log("test");
+
+		Entity entity = (Entity)target;
+
+		if (GUILayout.Button("Create Modules"))
+		{
+			entity.CreateModules();
+		}
+	}
+}
---
+++ CM/Scripts/CM/Essentials/Entity.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CM.Essentials
+{
+	public abstract class Entity : MonoBehaviour
+	{
+		[Header("Entity")]
+
+		[SerializeField]
+		private List<string> _moduleInterfaceNames;
+
+		public void CreateModules()
+		{
+			GameObject moduleParent = new GameObject();
+			moduleParent.transform.parent = transform;
+			moduleParent.name = "Modules";
+
+			for (int i = 0; i < _moduleInterfaceNames.Count; i++)
+			{
+				GameObject moduleObject = new GameObject();
+				moduleObject.transform.parent = moduleParent.transform;
+				moduleObject.name = _moduleInterfaceNames[i].Remove(0, 1) + "_Module";
+			}
+		}
+	}
+}
