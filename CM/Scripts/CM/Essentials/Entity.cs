fd0375b6b3243d164cf489943dc2f1927dd4c673
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-07-12 19:56:21 +0200

    Entity will now destroy all modules before creating new modules

--- CM/Scripts/CM/Essentials/Entity.cs
+++ CM/Scripts/CM/Essentials/Entity.cs
@@ -12,15 +12,30 @@ namespace CM.Essentials
 
 		public void CreateModules()
 		{
+			string moduleParentName = "Modules";
+
+			// Destroy Module Parent if exists
+			Transform[] children = gameObject.GetComponentsInChildren<Transform>();
+			foreach (Transform child in children)
+			{
+				if (child.name == moduleParentName)
+				{
+					DestroyImmediate(child.gameObject);
+					break;
+				}
+			}
+
+			// Create Module Parent
 			GameObject moduleParent = new GameObject();
 			moduleParent.transform.parent = transform;
-			moduleParent.name = "Modules";
+			moduleParent.name = moduleParentName;
 
+			// Create all Modules
 			for (int i = 0; i < _moduleInterfaceNames.Count; i++)
 			{
 				GameObject moduleObject = new GameObject();
 				moduleObject.transform.parent = moduleParent.transform;
-				moduleObject.name = _moduleInterfaceNames[i].Remove(0, 1) + "_Module";
+				moduleObject.name = _moduleInterfaceNames[i].Remove(0, 1) + " _Module";
 			}
 		}
 	}
