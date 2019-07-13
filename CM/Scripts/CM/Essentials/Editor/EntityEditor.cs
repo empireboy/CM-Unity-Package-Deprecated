41e8c40cd8c1b79a3444007f5b657b1d76aa803b
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-07-14 01:40:48 +0200

    Used Types instead of strings for interfaces in the Entity class

--- CM/Scripts/CM/Essentials/Editor/EntityEditor.cs
+++ CM/Scripts/CM/Essentials/Editor/EntityEditor.cs
@@ -5,15 +5,23 @@ using UnityEngine;
 [CustomEditor(typeof(Entity), true)]
 public class EntityEditor : Editor
 {
+	private float _spaceSize = 20f;
+
 	public override void OnInspectorGUI()
 	{
 		base.OnInspectorGUI();
 
 		Entity entity = (Entity)target;
 
+		// Create Modules
 		if (GUILayout.Button("Create Modules"))
 		{
 			entity.CreateModules();
 		}
+
+		GUILayout.Space(_spaceSize);
+
+		// Found Modules
+
 	}
 }
--- CM/Scripts/CM/Essentials/Entity.cs
+++ CM/Scripts/CM/Essentials/Entity.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 namespace CM.Essentials
@@ -7,36 +7,70 @@ namespace CM.Essentials
 	{
 		[Header("Entity")]
 
-		[SerializeField]
-		private List<string> _moduleInterfaceNames;
+		private Type[] _moduleInterfaceNames;
+		public Type[] ModuleInterfaceNames { get => _moduleInterfaceNames; }
+
+		private string _moduleParentName = "Modules";
+		private string _moduleSuffix = "_Module";
 
 		public void CreateModules()
 		{
-			string moduleParentName = "Modules";
-
 			// Destroy Module Parent if exists
-			Transform[] children = gameObject.GetComponentsInChildren<Transform>();
-			foreach (Transform child in children)
-			{
-				if (child.name == moduleParentName)
-				{
-					DestroyImmediate(child.gameObject);
-					break;
-				}
-			}
+			DestroyImmediate(GetParentModuleObject());
 
 			// Create Module Parent
 			GameObject moduleParent = new GameObject();
 			moduleParent.transform.parent = transform;
-			moduleParent.name = moduleParentName;
+			moduleParent.name = _moduleParentName;
+
+			// Get interfaces
+			_moduleInterfaceNames = this.GetType().GetInterfaces();
 
 			// Create all Modules
-			for (int i = 0; i < _moduleInterfaceNames.Count; i++)
+			for (int i = 0; i < _moduleInterfaceNames.Length; i++)
 			{
 				GameObject moduleObject = new GameObject();
 				moduleObject.transform.parent = moduleParent.transform;
-				moduleObject.name = _moduleInterfaceNames[i].Remove(0, 1) + " _Module";
+				moduleObject.name = _moduleInterfaceNames[i].ToString().Remove(0, 1) + _moduleSuffix;
 			}
+		}
+
+		public GameObject GetModuleObject<T>()
+		{
+			GameObject parentModule = GetParentModuleObject();
+
+			Transform[] children = parentModule.GetComponentsInChildren<Transform>();
+			foreach (Transform child in children)
+			{
+				if (child.name == (typeof(T).ToString().Remove(0, 1) + _moduleSuffix))
+				{
+					return child.gameObject;
+				}
+			}
+
+			return null;
+		}
+
+		private GameObject GetParentModuleObject()
+		{
+			Transform[] children = gameObject.GetComponentsInChildren<Transform>();
+			foreach (Transform child in children)
+			{
+				if (child.name == _moduleParentName)
+				{
+					return child.gameObject;
+				}
+			}
+
+			return null;
+		}
+
+		public T[] GetModules<T>()
+		{
+			GameObject moduleObject = GetModuleObject<T>();
+			T[] modules = moduleObject.GetComponents<T>();
+
+			return modules;
 		}
 	}
 }
