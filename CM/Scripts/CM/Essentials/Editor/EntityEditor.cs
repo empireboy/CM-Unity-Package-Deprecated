e59b385be16f309d1f502cbc5e0b3b556d05bb7e
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-07-14 17:32:34 +0200

    Fixed an Entity null reference for empty interface array

--- CM/Scripts/CM/Essentials/Editor/EntityEditor.cs
+++ CM/Scripts/CM/Essentials/Editor/EntityEditor.cs
@@ -11,7 +11,15 @@ public class EntityEditor : Editor
 	{
 		base.OnInspectorGUI();
 
+		// Only run in Editor mode while not playing
+		if (Application.isPlaying)
+			return;
+
+		// Get the target entity
 		Entity entity = (Entity)target;
+
+		// Entity Label
+		GUILayout.Label("Modules", EditorStyles.boldLabel);
 
 		// Create Modules
 		if (GUILayout.Button("Create Modules"))
@@ -19,9 +27,27 @@ public class EntityEditor : Editor
 			entity.CreateModules();
 		}
 
+		// Only run if there are modules created
+		if (entity.ModuleInterfaces == null)
+			return;
+
 		GUILayout.Space(_spaceSize);
 
 		// Found Modules
+		int modulesFoundNumber = 0;
 
+		for (int i = 0; i < entity.ModuleInterfaces.Length; i++)
+		{
+			Component[] modules = entity.GetModules(entity.ModuleInterfaces[i]);
+
+			foreach (Component module in modules)
+			{
+				GUILayout.Label(module.ToString());
+				modulesFoundNumber++;
+			}
+		}
+
+		// Display a Label with: "Modules Found (modulesFoundNumber)"
+		GUILayout.Label("Modules Found " + "(" + modulesFoundNumber + ")", EditorStyles.boldLabel);
 	}
 }
--- CM/Scripts/CM/Essentials/Entity.cs
+++ CM/Scripts/CM/Essentials/Entity.cs
@@ -3,15 +3,23 @@ using UnityEngine;
 
 namespace CM.Essentials
 {
+	[ExecuteInEditMode]
 	public abstract class Entity : MonoBehaviour
 	{
 		[Header("Entity")]
 
-		private Type[] _moduleInterfaceNames;
-		public Type[] ModuleInterfaceNames { get => _moduleInterfaceNames; }
+		private Type[] _moduleInterfaces;
+		public Type[] ModuleInterfaces { get => _moduleInterfaces; }
 
 		private string _moduleParentName = "Modules";
 		private string _moduleSuffix = "_Module";
+
+		private void Awake()
+		{
+			// Get Modules, check if the module parent exists
+			if (GetParentModuleObject())
+				InitializeModules();
+		}
 
 		public void CreateModules()
 		{
@@ -24,15 +32,20 @@ namespace CM.Essentials
 			moduleParent.name = _moduleParentName;
 
 			// Get interfaces
-			_moduleInterfaceNames = this.GetType().GetInterfaces();
+			InitializeModules();
 
 			// Create all Modules
-			for (int i = 0; i < _moduleInterfaceNames.Length; i++)
+			for (int i = 0; i < _moduleInterfaces.Length; i++)
 			{
 				GameObject moduleObject = new GameObject();
 				moduleObject.transform.parent = moduleParent.transform;
-				moduleObject.name = _moduleInterfaceNames[i].ToString().Remove(0, 1) + _moduleSuffix;
+				moduleObject.name = _moduleInterfaces[i].ToString().Remove(0, 1) + _moduleSuffix;
 			}
+		}
+
+		private void InitializeModules()
+		{
+			_moduleInterfaces = this.GetType().GetInterfaces();
 		}
 
 		public GameObject GetModuleObject<T>()
@@ -43,6 +56,22 @@ namespace CM.Essentials
 			foreach (Transform child in children)
 			{
 				if (child.name == (typeof(T).ToString().Remove(0, 1) + _moduleSuffix))
+				{
+					return child.gameObject;
+				}
+			}
+
+			return null;
+		}
+
+		public GameObject GetModuleObject(Type moduleInterface)
+		{
+			GameObject parentModule = GetParentModuleObject();
+
+			Transform[] children = parentModule.GetComponentsInChildren<Transform>();
+			foreach (Transform child in children)
+			{
+				if (child.name == (moduleInterface.ToString().Remove(0, 1) + _moduleSuffix))
 				{
 					return child.gameObject;
 				}
@@ -69,6 +98,14 @@ namespace CM.Essentials
 		{
 			GameObject moduleObject = GetModuleObject<T>();
 			T[] modules = moduleObject.GetComponents<T>();
+
+			return modules;
+		}
+
+		public Component[] GetModules(Type moduleInterface)
+		{
+			GameObject moduleObject = GetModuleObject(moduleInterface);
+			Component[] modules = moduleObject.GetComponents(moduleInterface);
 
 			return modules;
 		}
