6409ab3c1f679914c9c420f3d2d16a69389d3ddc
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-08-03 16:39:43 +0200

    Added Manager class

---
+++ CM/Scripts/CM/Essentials/Manager.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CM.Essentials
+{
+	public class Manager<T> : MonoBehaviour where T : class
+	{
+		public List<T> interfaces;
+	}
+}
