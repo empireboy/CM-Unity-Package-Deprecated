cc7042ed8da4ee55a9eec9d5277acf8bc405060f
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-09-03 10:32:47 +0200

    Added a LookAtPosition class

---
+++ CM/Scripts/CM/Orientation/LookAtPosition.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace CM.Orientation
+{
+	public class LookAtPosition : MonoBehaviour, IRotateTo
+	{
+		[SerializeField]
+		Transform _lookingTransform;
+
+		public void RotateTo(Vector3 position)
+		{
+			_lookingTransform.LookAt(position);
+		}
+	}
+}
