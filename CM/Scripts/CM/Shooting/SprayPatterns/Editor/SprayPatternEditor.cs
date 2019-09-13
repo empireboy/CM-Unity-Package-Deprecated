c219436eac226252960975e72754f0242c20af80
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-09-13 13:16:06 +0200

    Creating a spray pattern

---
+++ CM/Scripts/CM/Shooting/SprayPatterns/Editor/SprayPatternEditor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace CM.Shooting
+{
+	[CustomEditor(typeof(SprayPattern))]
+	public class SprayPatternEditor : Editor
+	{
+		private float _scale = 10f;
+
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			SprayPattern sprayPattern = (SprayPattern) target;
+
+			GUI.backgroundColor = Color.gray;
+			GUI.Box(new Rect(50, 120, 400, 400), "Spray Pattern Visualizer");
+
+			for (int i = 0; i < sprayPattern.sprayPoints.Count; i++)
+			{
+				EditorGUI.DrawRect(new Rect(50 + 400 / 2 + sprayPattern.sprayPoints[i].x * _scale, 120 + 400 / 2 + sprayPattern.sprayPoints[i].y * _scale, 10, 10), Color.black);
+			}
+		}
+	}
+}
---
+++ CM/Scripts/CM/Shooting/SprayPatterns/SprayPattern.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CM.Shooting
+{
+	[CreateAssetMenu(menuName = "CM/Shooting/SprayPattern", order = 2)]
+	public class SprayPattern : ScriptableObject
+	{
+		public List<Vector2> sprayPoints = new List<Vector2>();
+	}
+}
