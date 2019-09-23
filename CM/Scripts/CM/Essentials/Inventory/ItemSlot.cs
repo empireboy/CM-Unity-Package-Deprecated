e0369f72cfe89511858ea7b41d065cf3bcf0f321
DESKTOP-5SM091D\cemel <cemeloen@gmail.com>
2019-09-23 15:07:28 +0200

    Added a 3D Inventory example scene

--- CM/Scripts/CM/Essentials/Inventory/ItemSlot.cs
+++ CM/Scripts/CM/Essentials/Inventory/ItemSlot.cs
@@ -7,11 +7,12 @@ namespace CM.Essentials.Inventory
 		public int index = 0;
 		public ItemInstance itemInstance = null;
 		public GameObject prefabInstance = null;
+		public Transform slotTransform;
 
 		public void SetItem(ItemInstance itemInstance)
 		{
 			this.itemInstance = itemInstance;
-			this.prefabInstance = Instantiate(itemInstance.item.physicalRepresentation, transform);
+			this.prefabInstance = Instantiate(itemInstance.item.physicalRepresentation, slotTransform);
 		}
 
 		public void RemoveItem()
